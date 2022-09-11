55044d533c1ebf5a4fb30f549a1466d143caf098
EarthToAccess <oldmonkey333@gmail.com>
2022-09-11 05:05:40 -0400

    ported files from EBGui to work with Vowels; encoded doesn't exist just yet

---
+++ bin/info.lua
@@ -0,0 +1,6 @@
+-- General service information
+
+status = "Green"
+current = "v0"
+last = "v0"
+repo = "https://github.com/EarthToAccess/Vowels/"
---
+++ bin/main.lua
@@ -0,0 +1,10 @@
+local en = loadstring(game:HttpGet('https://raw.githubusercontent.com/EarthToAccess/Vowels/main/bin/en.lua'))()
+local ndec = loadstring(game:HttpGet('https://raw.githubusercontent.com/EarthToAccess/Vowels/main/bin/ndec.lua'))()
+
+xpcall(function()
+    local dec = ndec.decode(encui)
+    loadstring(dec)()
+end, function(e)
+    print('encountered error. xpcall says:', e)
+    warn('traceback: ',debug.traceback())
+end)
